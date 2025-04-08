1ab8b4dc9af15c286d804ef7272462dee4c31da8
kgchinthana <kavindugchinthana214@gmail.com>
2025-04-08 21:00:46 +0530

    Initial Commit

---
+++ src/main/java/com/codejam/codex/authzen/AuthzenApplication.java
@@ -0,0 +1,13 @@
+package com.codejam.codex.authzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthzenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codejam/codex/authzen/AuthzenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codejam.codex.authzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
